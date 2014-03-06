09cbda1d87e3e934d8c4a50dd07f87c578eb9785
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2014-03-05 22:25:17 -0500

    First with code.

--- README.md
+++ README.md
@@ -1,4 +1,106 @@
 Listular
 ========
 
-AngularJS Directive to help with rendering lists of data.
+**Listular** is an AngularJS directive that takes an array of data and renders it as a paged / sorted / filtered ng-repeat. 
+
+*This is very much a work in progress -- any feedback would be appreciated!*
+
+Say you have an array of data:
+ 
+```
+  {name: 'Satya Nadella', company: 'Microsoft'},
+  {name: 'Shantanu Narayen', company: "Adobe Systems"},
+  {name: 'Jeff Bezos', company: 'Amazon'},
+  {name: 'Rory Read', company: 'AMD'},
+```
+
+And you want to render this list in a typical `ng-repeat` but you also want paging/sorting/filtering... That's where **listular** comes in! 
+
+ 
+
+## Usage ##
+1. Add listular.js as a reference to your project `<script src="listular.js"></script>`
+2. Inject into your angular app `var app = angular.module('app', ['listular']);` 
+3. Create JavaScript object with Listular properties and data array `var listProperties = { data: [...], sortFields:[...]};`
+4. Add listular directive to your page `<div list data="listProperties" templateurl="template/person.html"></div>`
+
+##Customization##
+
+There are a number of options for customizing Listular (most of which can be passed in via attributes on the html or properties in the JavaScript object)
+
+
+* *templateurl* - The url of the main template for displaying the data
+* *showPageSize* - When true (default) shows a select box with the amount of results to show per page
+* *showNextPrevious* - When true (default) shows the next / previous / first / last buttons 
+* *showFilter* - When true (default) shows an input box above the list of data for filtering the data
+* *showSort* - When true (default) shows the available sort options
+
+In addition to the properties you can override the various parts of listular with your own template definitions by overriding the default value.
+
+```
+app.value('filterTemplate', {
+    template: '<h2>Add header text for some reasonx</h2><div class="filter"><input ng-model="query" ng-show="showFilter" placeholder="Filter"/></div>',
+    templateUrl: ''
+});
+```
+
+###Styling###
+There is no default style with Listular. You define the styles of listular to match your site. There are four css classes that correspond to the various parts of Listular
+
+1. *.filter* - The class that corresponds to the filter section
+2. *.sort* - The class that corresponds to the sorting properties. When a sort item selected it gets a class of `selected` applied to the list element, as well as whether or not the sorting is reversed (You can add pseudo-elements to give a visual indication as to which direction the sort is occurring -- check out the styles in the example to see this in action).
+3. *.listrepeat* - The class that corresponds to the repeat section
+4. *.paging* - The class that corresponds to the paging area at the bottom of the list
+
+##Example Usage##
+
+```
+<style>
+	.sort li.selected:after{
+		content: '˄';
+	}
+
+	.sort li.selected.reverse:after{
+		content: '˅';
+	}
+</style>
+<script type="text/javascript">
+	var listProperties = {
+		data: [
+	      {name: 'Satya Nadella', company: 'Microsoft'},
+	      {name: 'Shantanu Narayen', company: "Adobe Systems"},
+	      {name: 'Jeff Bezos', company: 'Amazon'},
+	      {name: 'Rory Read', company: 'AMD'},
+	      {name:'Steven Elop', company: 'Nokia'},
+	      {name:'Alan Mulally', company: 'Ford'},
+	      {name:'Tim Cook', company: 'Apple'},
+	      {name:'Michael Dell', company: 'Dell'},
+	      {name:'Mark Zuckerberg', company: 'Facebook'},
+	      {name:'Mikael Ohlsson', company: 'IKEA'},
+	      {name:'Paul Otellini', company: 'Intel'},
+	      {name:'Mitchell Baker', company: 'Mozilla'},
+	      {name:'Satoru Iwata', company: 'Nintendo'},
+	      {name:'Larry Ellison', company: 'Oracle'},
+	      {name:'Howard Schultz', company: 'Starbucks'},
+	      {name:'Gabe Newell', company: 'Valve'},
+	    ],
+	     sortFields: ['name', 'company'],
+	     templateUrl: "template/person.html",
+	     showFilter: true,
+	     showNextPrevious: true,
+	     showPageSize: true 
+	};	
+</script>
+...
+<body>
+	<div list data="listProperties"></div>
+</body>
+
+```
+
+
+##Roadmap##
+1. Data from controllers instead of random JavaScript object
+1. Unit tests
+1. Show page number
+1. Infinite scroll
---
+++ src/listular.js
@@ -0,0 +1,164 @@
+var app = angular.module('listular', []);
+
+//filter from here: 
+//http://stackoverflow.com/questions/14198822/angularjs-paging-orderby-only-affects-displayed-page
+app.filter('startFrom', function () {
+    return function (input, start) {
+        if (typeof input != 'undefined' && input != null) {
+            start = +start; //parse to int
+            return input.slice(start);
+        }
+        return input;
+    };
+});
+
+/*These are the default templates for the various parts. 
+  Using values so anyone implementing these can change out the templates as they see fit.
+*/
+app.value('filterTemplate', {
+    template: '<div class="filter"><input ng-model="query" ng-show="showFilter" placeholder="Filter"/></div>',
+    templateUrl: ''
+});
+
+
+app.value('sortTemplate', {
+    template: '<div class="sort" ng-show="showSort"><ul><li ng-repeat="sortItem in sortFields" ng-class="sortClass(sortItem)"><a href="#" ng-click="changeSortField(sortItem)">{{sortItem}}</li></ul></div>',
+    templateUrl: ''
+});
+
+app.value('repeatTemplate', {
+    template: '<div ng-repeat="item in filteredData | orderBy:sortField:sortReverse | startFrom:currentPage*pageSize | limitTo:pageSize" class="listrepeat">' +
+                       '<div ng-switch on="templateurl">' +
+                            '<div ng-switch-when="">{{item}}</div>' +
+                            '<div ng-switch-default><div ng-include="templateurl" class="list-row"></div></div>' +
+                        '</div>' +
+                    '</div>',
+    templateUrl: ''
+});
+
+app.value('nextPreviousTemplate', {
+    template: '<div ng-show="showNextPrevious" class="paging">' +
+                            '<button ng-click="first()">&lt;&lt;</button>' +
+                            '<button ng-click="prev()">&lt;</button>' +
+                            '<select ng-model="pageSize" ng-show="showPageSize">' +
+                                '<option>3</option>' +
+                                '<option>5</option>' +
+                                '<option>8</option>' +
+                            '</select>' +
+                            '<button ng-click="next()">&gt;</button>' +
+                            '<button ng-click="last()">&gt;&gt;</button>' +
+                         '</div>',
+    templateUrl: ''
+});
+''
+app.service('templates',['filterTemplate', 'sortTemplate', 'repeatTemplate', 'nextPreviousTemplate', function(filterTemplate, sortTemplate, repeatTemplate, nextPreviousTemplate){
+    this.getTemplates = function(){
+       return filterTemplate.template + sortTemplate.template + repeatTemplate.template + nextPreviousTemplate.template;
+    }
+}]);
+
+app.directive('list', ['$filter', 'templates', function ($filter, templates) {
+    return {
+        template: templates.getTemplates(),
+        link: function (scope, elem, attrs) {
+            //defaults
+            scope.templateurl = "";
+            scope.currentPage = 0;
+            scope.pageSize = 3;
+            scope.query = "";
+            scope.showNextPrevious = true;
+            scope.showPageSize = true;
+            scope.showFilter = true;
+            scope.showSort = true;
+            scope.sortField = "name";
+            scope.sortReverse = false;
+
+            if ('undefined' != typeof attrs["data"]) {
+                //do something to make it so we can pass this in on scope as well
+                scope.data = window[attrs["data"]].data;
+                scope.filteredData = scope.data;
+                scope.sortFields = window[attrs["data"]].sortFields;
+                scope.templateUrl = window[attrs["data"]].templateUrl || "";
+                scope.showPageSize = window[attrs["data"]].showPageSize || scope.showPageSize; 
+                scope.showNextPrevious = window[attrs["data"]].showNextPrevious || scope.showNextPrevious; 
+                scope.showFilter = window[attrs["data"]].showFilter || scope.showFilter; 
+                scope.showSort = window[attrs["data"]].showSort || scope.showSort; 
+                scope.sortField = window[attrs["data"]].sortField || scope.sortField; 
+            }
+
+            if ('undefined' != typeof attrs["templateurl"]) {
+                scope.templateurl = attrs["templateurl"];
+            }
+
+            if ('undefined' != typeof attrs["showPageSize"]) {
+                scope.showPageSize = attrs["showPageSize"] == "true";
+            }
+
+            if ('undefined' != typeof attrs["showNextPrevious"]) {
+                scope.showNextPrevious = attrs["showNextPrevious"] == "true";
+            }
+
+            if ('undefined' != typeof attrs["showFilter"]) {
+                scope.showFilter = attrs["showFilter"] == "true";
+            }
+
+            if ('undefined' != typeof attrs["showSort"]) {
+                scope.showSort = attrs["showSort"] == "true";
+            }
+
+            if ('undefined' != typeof attrs["defaultSort"]) {
+                scope.sortField = attrs["defaultSort"];
+            }
+
+
+            scope.numberOfPages = function () {
+                return Math.ceil(scope.filteredData.length / scope.pageSize);
+            };
+
+            scope.changeSortField = function (item) {
+                if (item == null) return; 
+                if (scope.sortField == item) {
+                    scope.sortReverse = !scope.sortReverse;
+                }
+
+                scope.sortField = item;
+            };
+
+            scope.sortClass = function(item) {
+                return item == scope.sortField ?
+                    scope.sortReverse == true ? "selected reverse" : "selected"
+                    : "";
+            };
+
+            scope.first = function () {
+                scope.currentPage = 0;
+            };
+
+            scope.last = function () {
+                scope.currentPage = scope.numberOfPages() - 1;
+            };
+
+            scope.prev = function () {
+                scope.currentPage = scope.currentPage == 0 ? 0 : scope.currentPage - 1;
+            };
+
+            scope.next = function () {
+                scope.currentPage = scope.currentPage == scope.numberOfPages() - 1 ? scope.currentPage : scope.currentPage + 1;
+            };
+
+            //go to page 1 when filtering
+            scope.$watch('query', function (val, orig) {
+                scope.currentPage = 0;
+                scope.filteredData = $filter("filter")(scope.data, scope.query);
+            });
+
+            //make sure we don't go outside of max pages
+            //also eventually want to make this not happen as a watch for performance
+            scope.$watch('numberOfPages()', function (val, orig) {
+                if (scope.currentPage >= scope.numberOfPages()) {
+                    scope.currentPage = scope.numberOfPages() - 1;
+                }
+            });
+        }
+    };
+}]);
